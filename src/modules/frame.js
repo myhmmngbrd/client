8ff4c4889b7595134f5da5d7c7473aa99c6260f7
a <myhmmngbrd@gmail.com>
2020-06-15 17:49:25 +0900

    voca

---
+++ src/modules/frame.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Frame extends React.Component {
+    render() {
+        return (
+            <div>
+                New Frame
+            </div>
+        )
+    }
+}
+
+export default Frame;
---
+++ src/modules/sql_form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Contents extends React.Component {
+    render() {
+        return (
+            <form>
+
+            </form>
+        )
+    }
+}
